fdd2831f780b1739f96f495cbc34685cc5918aca
Krzysztof Nasuta <47270121+Nasus20202@users.noreply.github.com>
2022-07-29 22:47:27 +0200

    Update app.js

--- TokenGenerator/app.js
+++ TokenGenerator/app.js
@@ -134,6 +134,7 @@ app.get('/refresh_token', function(req, res) {
   };
 
   request.post(authOptions, function(error, response, body) {
+    console.log(body)
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
