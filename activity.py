3b3d9e52ab73cb68522cd4a4bae57c6bad62c49a
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2022-09-07 00:25:33 +0200

    Discord status optimalization

--- activity.py
+++ activity.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv()
 
+current_status = "status"
 
 headers = {
         'Content-Type': 'application/json',
@@ -11,6 +12,9 @@ headers = {
     }
 
 def set_status_without_emoji(status):
+    global current_status
+    if(current_status == status):
+        return
     data = '{"custom_status":{"text":"'+str(status)+'"}}'
     try:
         response = requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, data=data, timeout=3)
@@ -18,6 +22,9 @@ def set_status_without_emoji(status):
         print("Cannot change the Discord status!")
 
 def set_status(status, emoji):
+    global current_status
+    if(status == current_status):
+        return
     data = '{"custom_status":{"text":"'+str(status)+'", "emoji_name":"'+emoji+'"}}'
     try:
         response = requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, data=data.encode('utf-8'), timeout=3)
